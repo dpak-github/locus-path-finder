46bbc49a8ae0faa2b85bb15b687f9278665828db
Deepak Shinde <deepak.shinde@neptune-ubi.com>
2021-01-10 19:40:00 +0530

    [* master]: added swagger

--- src/main/java/com/locus/path/points/controller/MapController.java
+++ src/main/java/com/locus/path/points/controller/MapController.java
@@ -6,26 +6,26 @@ import com.google.maps.model.LatLng;
 import com.locus.path.points.service.PathPointService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
-@RestController("/api/maps")
+@RestController
+@RequestMapping("/api/maps")
 @AllArgsConstructor
 public class MapController
 {
     private final PathPointService pathPointService;
 
-    @GetMapping("/paths")
-    public List<LatLng> getIntermediatePathPoints(final List<String> source, final List<String> destination)
+    @GetMapping("/path/points")
+    public List<LatLng> getIntermediatePathPoints(@RequestParam(required = true) final String sourceLat, @RequestParam(required = true) final String sourceLang,
+            @RequestParam(required = true) final String destLat, @RequestParam(required = true) final String destLang)
     {
-        if (source.size() != 2 || destination.size() != 2)
-        {
-            throw new RuntimeException("Please give latitude and longitude values for source and destination");
-        }
 
-        LatLng sourcePoint = new LatLng(Double.parseDouble(source.get(0)), Double.parseDouble(source.get(1)));
-        LatLng destinationPoint = new LatLng(Double.parseDouble(destination.get(0)), Double.parseDouble(destination.get(1)));
+        LatLng sourcePoint = new LatLng(Double.parseDouble(sourceLat), Double.parseDouble(sourceLang));
+        LatLng destinationPoint = new LatLng(Double.parseDouble(destLat), Double.parseDouble(destLang));
         return pathPointService.getAllPointsUsingDirectionApi(sourcePoint, destinationPoint);
     }
 }
